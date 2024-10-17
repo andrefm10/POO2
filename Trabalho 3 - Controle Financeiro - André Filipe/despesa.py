d571d4603f26102233399bcf556a12bac4008c85
andrefm10 <andrefilipmartins@gmail.com>
2024-10-17 10:25:18 -0300

    Add files via upload

---
+++ Trabalho 3 - Controle Financeiro - André Filipe/despesa.py
@@ -0,0 +1,9 @@
+
+class Despesa:
+    def __init__(self, valor, categoria, data, descricao):
+        self.valor = valor
+        self.categoria = categoria
+        self.data = data
+        self.descricao = descricao
+
+
